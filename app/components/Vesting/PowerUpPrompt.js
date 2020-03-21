56564216a10fd0c9218c9570442ccc4051bfbefe
Andrew Chaney (netuoso) <andrewc@pobox.com>
2020-03-21 12:54:34 -0500

    Fix issue with power up float

--- app/components/Vesting/PowerUpPrompt.js
+++ app/components/Vesting/PowerUpPrompt.js
@@ -65,7 +65,7 @@ class PowerUpPrompt extends Component {
   }
 
   handleOnChange = (value) => {
-    const hiveAmount = parseFloat(value);
+    const hiveAmount = parseFloat(value).toFixed(3);
     // const props = this.props.hive.props;
     this.setState({ hiveAmount });
   }
